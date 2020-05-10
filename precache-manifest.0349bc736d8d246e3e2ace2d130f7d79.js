1cf58d7dcdff7ec218cce35e1d5de533eeee7e21
Lin,Ting Wei <lin84william@gmail.com>
2020-05-10 17:13:37 +0800

    測試

---
+++ precache-manifest.0349bc736d8d246e3e2ace2d130f7d79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc724304de9ae813e9f",
+    "url": "/mychart/css/app.ca936927.css"
+  },
+  {
+    "revision": "5703ff2ceed5ec526c84",
+    "url": "/mychart/css/chunk-vendors.6af9d10e.css"
+  },
+  {
+    "revision": "87f1689ba979e0ab64df99918f89bfe8",
+    "url": "/mychart/index.html"
+  },
+  {
+    "revision": "cfc724304de9ae813e9f",
+    "url": "/mychart/js/app.c3ee5fa5.js"
+  },
+  {
+    "revision": "5703ff2ceed5ec526c84",
+    "url": "/mychart/js/chunk-vendors.0e104ed2.js"
+  },
+  {
+    "revision": "5d567d51092fc20baabdf57600bc7b39",
+    "url": "/mychart/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mychart/robots.txt"
+  }
+]);
