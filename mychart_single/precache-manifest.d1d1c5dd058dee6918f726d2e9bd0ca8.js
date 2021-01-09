796ef3626701d4accc741a179fc7307c92a47087
Ting-Wei <lin84william@gmail.com>
2021-01-09 17:11:14 +0800

    20210109 單間教室

---
+++ mychart_single/precache-manifest.d1d1c5dd058dee6918f726d2e9bd0ca8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5489bb07ef8e844c911e77987d99adca",
+    "url": "/mychart_single/big-data.svg"
+  },
+  {
+    "revision": "d7afb57dc82ff903135e",
+    "url": "/mychart_single/css/app.b09c5dc2.css"
+  },
+  {
+    "revision": "12fc2c8baeeb8a9e3f65",
+    "url": "/mychart_single/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "03a4792ba3b63d105d2511dc3f246eb2",
+    "url": "/mychart_single/index.html"
+  },
+  {
+    "revision": "d7afb57dc82ff903135e",
+    "url": "/mychart_single/js/app.7f23f017.js"
+  },
+  {
+    "revision": "12fc2c8baeeb8a9e3f65",
+    "url": "/mychart_single/js/chunk-vendors.33873b8b.js"
+  },
+  {
+    "revision": "65fe7b214cf992ec3e373c3bdf5672a7",
+    "url": "/mychart_single/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/mychart_single/robots.txt"
+  }
+]);
---
+++ mychart_single/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mychart_single/precache-manifest.d1d1c5dd058dee6918f726d2e9bd0ca8.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "chart_single"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
